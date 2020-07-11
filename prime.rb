9c8f5656ee4ed62673a4ae5a0982a9d12f239d04
Milos Popovic <milos_popovic@outlook.com>
2020-07-11 15:53:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,9 +3,9 @@ def prime?(integer)
   number_range = (2..integer-1).to_a
   number_range.each do |i|
     if integer % i == 0
-      return false
-    else
       return true
+    else
+      return false
     end
   end
 end
