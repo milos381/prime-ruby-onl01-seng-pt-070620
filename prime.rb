d3d00ca9aa24b908a88250e126d2bcb6652e5f7d
Milos Popovic <milos_popovic@outlook.com>
2020-07-11 16:10:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(integer)
+ return false if integer < 2
+  number_range = (2..integer-1).to_a #a cool way to
+                                    #create range of an intiger
+  number_range.each do |i|
+    return false if integer % i == 0
+  end
+  true #after the loop has completed
+        #we know we've sucesffully looped through
+        #and found no numbers that can be factors
+        #so we need to return true at the very end
+        #because if we made it there....
+        #we know we have a prime number
+end
