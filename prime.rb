99cef2c992fe39374972a20c2393f138126a74fa
Milos Popovic <milos_popovic@outlook.com>
2020-07-11 16:10:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,8 +5,8 @@ def prime?(integer)
   number_range.each do |i|
     return false if integer % i == 0
   end
-  true #after the loop has completed 
-        #we know we've sucesffully looped through 
+  true #after the loop has completed
+        #we know we've sucesffully looped through
         #and found no numbers that can be factors
         #so we need to return true at the very end
         #because if we made it there....
